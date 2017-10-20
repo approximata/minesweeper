371d5d0541beac52339ddb6391c566b1ff91937f
Mate Bodor <bodormate@gmail.com>
2017-10-20 08:01:16 +0200

    refactor(drawing): change table to div

--- render.js
+++ render.js
@@ -1,19 +1,21 @@
 'use strict'
 
 export default function renderGame(gameRoot, gameState) {
-  const mainTable = document.createElement('table')
+  const mainTable = document.createElement('div')
+  mainTable.className = 'main-table'
   gameState.forEach(row => renderRow(mainTable, row))
   gameRoot.appendChild(mainTable)
 }
 
 function renderRow(mainTable, row) {
-  const tableRow = document.createElement('tr')
+  const tableRow = document.createElement('div')
+  tableRow.className = 'row'
   row.forEach(cell => renderCells(tableRow, cell))
   mainTable.appendChild(tableRow)
 }
 
 function renderCells(tableRow, cell) {
-  const tableCell = document.createElement('td')
-  tableCell.innerHTML = cell
+  const tableCell = document.createElement('div')
+  tableCell.className = 'cell ' + cell
   tableRow.appendChild(tableCell)
 }
